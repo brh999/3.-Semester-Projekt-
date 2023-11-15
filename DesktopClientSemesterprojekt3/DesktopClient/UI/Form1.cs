2212bba3a236816a82b1fa733374649b87898216
Mathias Rasmussen <mathias.l.m.rasmussen@gmail.com>
2023-11-15 11:26:06 +0100

    Implemented create buy order
    Implemented create buy order with some minimalist input validation

--- DesktopClientSemesterprojekt3/DesktopClient/UI/Form1.cs
+++ DesktopClientSemesterprojekt3/DesktopClient/UI/Form1.cs
@@ -1,10 +1,42 @@
+using DesktopClient.BusinessLogic;
+using Models;
+
 namespace DesktopClient
 {
     public partial class Form1 : Form
     {
+        private BidControl _bidControl;
         public Form1()
         {
             InitializeComponent();
+            _bidControl = new BidControl();
+            cmbCurrency.DataSource = Enum.GetValues(typeof(CurrencyEnum));
+        }
+
+        private void btnCreateBid_Click(object sender, EventArgs e)
+        {
+            int amount;
+            double price;
+
+            if (!int.TryParse(txtAmount.Text, out amount))
+            {
+                // TODO: inform user that something went wrong.
+            }
+
+            if (!double.TryParse(txtPrice.Text, out price))
+            {
+                // TODO: inform user that something went wrong.
+            }
+
+            
+
+            //_bidControl.CreateBid(amount, price);
+        }
+
+        private void cmbCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CurrencyEnum selectedValue = (CurrencyEnum)cmbCurrency.SelectedItem;
+
         }
     }
 }
