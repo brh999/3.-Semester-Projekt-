00602fd8ba05cd5e0eccb53dd4c9f8e8bc4e311d
burberrymyshirt <magnushlarsen01@gmail.com>
2023-11-15 11:56:27 +0100

    added some bid logic

---
+++ WebApiSemesterprojekt3/WebApi/BuissnessLogiclayer/BidLogic.cs
@@ -0,0 +1,17 @@
+﻿using WebApi.Model;
+
+namespace WebApi.BuissnessLogiclayer
+{
+    public class BidLogic : IBidLogic
+    {
+        public Bid GetAccountSpecificBids(int AccountFk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Bid> GetAllBids()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApiSemesterprojekt3/WebApi/Database/BidDBAccess.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+using WebApi.Model;
+
+namespace WebApi.Database
+{
+    public class BidDBAccess : IBidDBAccess
+    {
+        private IConfiguration _configuration { get; set; }
+
+        public BidDBAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IEnumerable<Bid> GetAllBids()
+        {
+            List<Account> res = null;
+            string? connectionString = _configuration.GetConnectionString("hildur");
+
+            //TODO lav "*" om til individuelle kollonner
+            string queryString = "select * from accounts";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                //TODO add error chekcing mayhaps?
+                res = conn.Query<Bid>(queryString).ToList();
+            }
+            return res;
+        }
+    }
