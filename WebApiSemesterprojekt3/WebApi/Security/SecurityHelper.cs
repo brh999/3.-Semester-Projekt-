833e4a41045cbc977e1cb0d63d8bbe0a2efdd82e
burberrymyshirt <magnushlarsen01@gmail.com>
2023-11-21 13:06:51 +0100

    jwt api initialize

---
+++ WebApiSemesterprojekt3/WebApi/Security/SecurityHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebApi.Security
+{
+    public class SecurityHelper
+    {
+        private readonly IConfiguration _configuration;
+        public SecurityHelper(IConfiguration inConfiguration)
+        {
+            _configuration = inConfiguration;
+        }
+
+        public SymmetricSecurityKey? GetSecurityKey()
+        {
+            SymmetricSecurityKey? SIGNING_KEY = null;
+            if (_configuration is not null)
+            {
+                string SECRET_KEY = _configuration["SECRET_KEY"];
+                SIGNING_KEY = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SECRET_KEY));
+            }
+            return SIGNING_KEY;
+        }
+
+        public bool IsValidUsernameAndPassword(string username, string password)
+        {
+            string allowedUsername = _configuration["AllowDesktopApp:Username"];
+            string allowedPassword = _configuration["AllowDesktopApp:Password"];
+            bool credentialsOk = (username.Equals(allowedUsername)) && (password.Equals(allowedPassword));
+            return credentialsOk;
+        }
+    }
+}
