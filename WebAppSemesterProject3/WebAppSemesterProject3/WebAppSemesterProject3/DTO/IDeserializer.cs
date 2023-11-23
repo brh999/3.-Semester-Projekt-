a6baafb32746417266b2c13d73a6059efa9e3684
MickeyMaa <117630962+MickeyMaa@users.noreply.github.com>
2023-11-23 10:20:33 +0100

    remove old

--- WebAppSemesterProject3/WebAppSemesterProject3/WebAppSemesterProject3/DTO/IDeserializer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebAppSemesterProject3.DTO
-{
-    public interface IDeserializer<T>
-    {
-        Task<T> GetObject(int id);
-        Task<IEnumerable<T>> GetList();
-    }
-}
--- WebAppSemesterProject3/WebAppSemesterProject3/WebAppSemesterProject3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppSemesterProject3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
