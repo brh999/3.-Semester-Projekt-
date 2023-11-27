a0de7a0e1fc1d46c99da4eaaaa9b4985fa379f25
MickeyMaa <117630962+MickeyMaa@users.noreply.github.com>
2023-11-27 09:29:50 +0100

    remove auth

--- WebAppSemesterProject3/WebAppSemesterProject3/WebAppWithAuthentication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppWithAuthentication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
