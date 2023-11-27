b0ed0a00118b558aead0d348d9b001d24316fbac
Magnus Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-11-27 07:00:10 +0100

    made it so it doesn't ask for a new jwt every time it calls api

--- DesktopClientSemesterprojekt3/DesktopClient/BusinessLogic/CurrencyLogic.cs
+++ DesktopClientSemesterprojekt3/DesktopClient/BusinessLogic/CurrencyLogic.cs
@@ -24,7 +24,7 @@ namespace DesktopClient.BusinessLogic
             List<Currency>? foundCurrencies = null;
 
             // Get token
-            TokenState currentState = TokenState.Invalid;        // Presumed state
+            TokenState currentState = TokenState.Valid;        // Presumed state
             string? tokenValue = await _tokenManager.GetToken(currentState);
             if (tokenValue != null)
             {
